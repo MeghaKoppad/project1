0f6f7ba1e9515d9335ba884b25b9ce0a5372f298
Meghu63613 <meghalk1999@gmail.com>
2023-05-27 10:42:14 +0530

    Hi

---
+++ src/main/java/basics1/Mobile.java
@@ -0,0 +1,12 @@
+package basics1;
+
+public class Mobile
+{
+	public Mobile() {
+		System.out.println("Object is created");
+	}
+ void musicPlayer() 
+ {
+	 System.out.println("Abc.......");
+ }
+}
---
+++ src/main/java/basics1/TestMobile.java
@@ -0,0 +1,32 @@
+package basics1;
+
+import org.springframework.beans.factory.BeanFactory;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestMobile {
+public static void main(String[] args) {
+//	ClassPathResource resource=new ClassPathResource("ioc1.xml");
+//	BeanFactory beanFactory=new XmlBeanFactory(resource);
+//	Mobile mobile=(Mobile) beanFactory.getBean("mymobile");
+//	mobile.musicPlayer();
+//	Mobile mobile1=(Mobile) beanFactory.getBean("mymobile");
+//	mobile1.musicPlayer();
+			
+	
+	
+	ApplicationContext context=new ClassPathXmlApplicationContext("ioc1.xml");
+	Mobile mobile=(Mobile) context.getBean("mymobile");
+	mobile.musicPlayer();
+			
+			
+			
+			
+}}
+		
+	
+
